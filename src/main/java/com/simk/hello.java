38b8d11d198bd044415375f889509973356b17cc
ai070736 <98873657+ai070736@users.noreply.github.com>
2022-08-24 15:29:50 +0800

    Update hello.java

--- src/main/java/com/simk/hello.java
+++ src/main/java/com/simk/hello.java
@@ -11,5 +11,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("simkbranch");
+        System.out.println("github 修改");
     }
 }
